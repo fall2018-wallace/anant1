3bf428f52c958f7ca6628558c08143c4ca8852a2
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-10 15:50:36 -0400

    Saving MergeArrest.R

--- MergeArrest.R
+++ MergeArrest.R
@@ -8,3 +8,4 @@ colnames(arrest) <- c("statename")
 arrest
 merged_data <- merge(census,arrest)
 merged_data
+colnames(merged_data) <- c("Statename","Murder","Assault","UrbanPop", "Rape","population","popover18", "percentover18")
