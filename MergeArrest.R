26b5ad59e42a39024f3e5bc660a53c7dc2c09c47
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-10 17:21:31 -0400

    Saving MergeArrest.R

--- MergeArrest.R
+++ MergeArrest.R
@@ -25,7 +25,7 @@ new=merged_data$population/merged_data$Murder
 no_of_murders=new/100000
 no_of_murders
 
-per_state_murder <- ggplot(merged_data,aes(x=Statename,y=no_of_murders))+geom_col()+theme(axis.text.x=element_text(angle=90, hjust=1))+ggtitle("Total murders")
+per_state_murder <- ggplot(merged_data,aes(x=Statename,y=no_of_murders))+geom_col()+theme(axis.text.x=element_text(angle=90, hjust=1))
 y <- ggplot(merged_data,aes(x=reorder(Statename, no_of_murders),y=no_of_murders))+geom_col()+theme(axis.text.x=element_text(angle=90, hjust=1))+ggtitle("Total murders")
 z <- ggplot(merged_data,aes(x=reorder(Statename, no_of_murders),y=no_of_murders))+geom_col(aes(fill=percentover18))+theme(axis.text.x=element_text(angle=90, hjust=1))+ggtitle("Total murders")
 
