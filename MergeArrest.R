5a5bbeb9c072fe513c0dcb90ad3655f98f0dac0c
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-10 15:53:08 -0400

    Saving MergeArrest.R

--- MergeArrest.R
+++ MergeArrest.R
@@ -10,3 +10,5 @@ merged_data <- merge(census,arrest)
 merged_data
 colnames(merged_data) <- c("Statename","Murder","Assault","UrbanPop", "Rape","population","popover18", "percentover18")
 merged_data
+
+ggplot(merged_data,aes(x=population))+geom_histogram(bins=50,color="black", fill="black")
