7c858968d0c86d72a70ad492c91b46172411638e
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-11 00:10:01 -0400

    Saving MergeArrest.R

--- MergeArrest.R
+++ MergeArrest.R
@@ -20,13 +20,15 @@ plot_population
 plot_murder <- ggplot(merged_data,aes(x=Murder))+geom_histogram(bins=50,color="black", fill="black")
 plot_murder 
 #4) Create a boxplot for the population, and a different boxplot for the murder rate.
+
 box_population <- ggplot(merged_data,aes(x=factor(0),y=population))+geom_boxplot()
 box_murder <- ggplot(merged_data,aes(x=factor(0),y=Murder))+geom_boxplot()
 
+#7) Calculate the number of murders per state
 new=merged_data$population*merged_data$Murder
 no_of_murders=new/100000
 no_of_murders
-
+#8) Generate a bar chart, with the number of murders per state
 per_state_murder <- ggplot(merged_data,aes(x=Statename,y=no_of_murders))+geom_col()+theme(axis.text.x=element_text(angle=180, hjust=1))
 rotate_title_bar <- ggplot(merged_data,aes(x=Statename,y=no_of_murders))+geom_col()+theme(axis.text.x=element_text(angle=90, hjust=1))+ggtitle("Total Murders")
 rotate_title_bar_sorted <- ggplot(merged_data,aes(x=reorder(Statename, no_of_murders),y=no_of_murders))+geom_col()+theme(axis.text.x=element_text(angle=90, hjust=1))+ggtitle("Total murders")
